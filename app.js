dfabb83da5fe0f997b75a58edfd9ed4683bd2bff
Vishal Ranjan <vishivish18@gmail.com>
2016-01-29 17:29:40 +0530

    CORS

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ var bodyParser = require('body-parser');
 var app = express();
 var Location = require('./app/models/location')
 //app.use(morgan('dev'));
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
+
 app.use('/api/route', router);
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({
